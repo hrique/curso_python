27438c381f5fc3503d3f5643b5e94c2203764fae
henriqueframeschi <66852208+hrique@users.noreply.github.com>
2025-05-18 16:39:16 -0300

    exercicio lista corrigido

--- python3_udemy/exercicio_lista_compras.py
+++ python3_udemy/exercicio_lista_compras.py
@@ -9,30 +9,29 @@ while True:
     opcao = input('Selecione uma opção\n' \
     '[i]nserir [a]pagar [l]istar:')
 
-
     # declara quais opções podem ser digitadas
     opcoes_validas = 'ial'
 
     # verifica se a opção esta nas opções validas
     if opcao not in opcoes_validas:
-        print('Digite uma opção válida!')
         os.system('cls')
-        continue
+        print('Digite uma opção válida!')
     
     # verifica se foi digitada apenas 1 opção
     if len(opcao) >1:
-        print('Digite apenas uma opção!')
         os.system('cls')
+        print('Digite apenas uma opção!')
+        
 
     # se a opção em minusculo é a letra 'i' executa a ação de adicionar um item a lista e limpa o terminal
     if opcao.lower() == 'i':
+        os.system('cls')
         novo_item = input('Digite o item para inserir na lista:')
         lista.append(novo_item)
-        os.system('cls')
-        continue
 
     # se a opção em minusculo é a letra 'a' tenta apagar o indice da lista que o usuario digitar
     elif opcao.lower() == 'a':
+        os.system('cls')
         for indice, item in enumerate(lista):
             print(indice, item)
         apagar_item = input('Digite o indice que gostaria de apagar da lista:')
@@ -40,13 +39,11 @@ while True:
             del lista[int(apagar_item)]
         except:
             print('Digite um indice valido!')
-            continue
-        os.system('cls')
 
     # se a opção em minusculo é a letra 'l' mostra os valores que estao na lista
     elif opcao.lower() == 'l':
+        os.system('cls')
         for indice, item in enumerate(lista):
             print(indice, item)
-            continue
 
     
