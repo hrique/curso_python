cf99210d0258b7c9af4f0c942de5f1b4d1266729
hrique <hrique.frameschi@gmail.com>
2025-05-21 17:21:41 -0300

    exercicio_convidados

--- intensivo_python/cap3/3_5_lista_convidados.py
+++ intensivo_python/cap3/3_5_lista_convidados.py
@@ -10,3 +10,10 @@ print(msg_conv_2)
 
 msg_conv_3 = f'Olá {convidados[0].title()} vamos sair para jantar?'
 print(msg_conv_3)
+
+print(f'Infelizmente o {convidados[1].title()}, não poderá comparecer ao Jantar.')
+
+convidados[1] = 'faker'
+
+msg_conv_4 = f'Olá {convidados[1].title()} gostaria de sair para jantar?'
+print(msg_conv_4)
