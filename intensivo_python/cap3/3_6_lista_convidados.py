bb503101d1a52621721d84aea4e0925e7f4d6b72
henriqueframeschi <66852208+hrique@users.noreply.github.com>
2025-06-07 10:32:18 -0300

    atual

--- intensivo_python/cap3/3_6_lista_convidados.py
+++ intensivo_python/cap3/3_6_lista_convidados.py
@@ -1,12 +1,12 @@
 convidados = ['einstein', 'tesla', 'fabio akita']
-lista_convidados = f'A lista de convidados tem as seguintes pessoas: {convidados}'
-print(lista_convidados)
+print(f'A lista de convidados tem as seguintes pessoas: {convidados}')
+
 
 print(f'\nOlá {convidados[1].title()} vamos sair para jantar?')
 
 print(f'\nOlá {convidados[2].title()} vamos sair para jantar?')
 
-print(msg_conv_3 = f'\nOlá {convidados[0].title()} vamos sair para jantar?')
+print(f'\nOlá {convidados[0].title()} vamos sair para jantar?')
 
 print(f'\nInfelizmente o {convidados[1].title()}, não poderá comparecer ao Jantar.')
 
